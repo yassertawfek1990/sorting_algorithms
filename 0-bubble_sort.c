9855885474287edb2426aaef9d8b89ea721dd1b4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-17 11:43:48 -0800

    c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swarray.
+ * @z: The egrto wap.
+ * @x: The r to swap.
+ */
+void swap(int *z, int *x)
+{
+	int t;
+
+	t = *z;
+	*z = *x;
+	*x = t;
+}
+
+/**
+ * bubble_sort - Sorrder.
+ * @array: Anrt.
+ * @size: Thsiz
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j;
+	size_t l;
+	bool bub;
+
+	l = size;
+	bub = false;
+	if (array == NULL || size < 2)
+		return;
+	while (bub == false)
+	{
+		bub = true;
+		for (j = 0; j < l - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array + j, array + j + 1);
+				print_array(array, size);
+				bub = false;
+			}
+		}
+		l--;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sorting_algorithms
+sortisac
